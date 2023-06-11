5dad045d9a1b9e2bb7cb951ff80f19cf51f63c3d
bobur2006coder <boburdilmurodov702@gmail.com>
2023-06-11 19:28:45 +0500

    fix

---
+++ index.js
@@ -0,0 +1,38 @@
+let one =document.getElementById('one')
+let two =document.getElementById('two')
+let three =document.getElementById('three')
+let four =document.getElementById('four')
+let five =document.getElementById('five')
+let six =document.getElementById('six')
+
+
+one.onclick=()=>{
+    let oneMusic= new Audio('Clapping Sound Effects.mp3')
+    oneMusic.play()
+}
+
+two.onclick=()=>{
+    let oneMusic= new Audio('HORSE - Sound Effect.mp3')
+    oneMusic.play()
+}
+
+three.onclick=()=>{
+    let oneMusic= new Audio('Trumpet sound effect.mp3')
+    oneMusic.play()
+}
+
+four.onclick=()=>{
+    let oneMusic= new Audio('Just do it Sound Effect.mp3')
+    oneMusic.play()
+}
+
+five.onclick=()=>{
+    let oneMusic= new Audio('Victory - Sound Effect.mp3')
+    oneMusic.play()
+}
+
+six.onclick=()=>{
+    let oneMusic= new Audio('Wrong Answer Sound effect.mp3')
+    oneMusic.play()
+}
+
